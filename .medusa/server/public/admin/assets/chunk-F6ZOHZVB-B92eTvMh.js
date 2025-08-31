abe3b033f077eb881a94bef4761ba92d43d653fb
adoel90 <adoel.cs@gmail.com>
2025-08-31 09:20:32 +0700

    chore: try to solve localhost part-2

---
+++ .medusa/server/public/admin/assets/chunk-F6ZOHZVB-B92eTvMh.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-4Ied8IC1.js";var e=({children:s})=>r.jsx("span",{className:"sr-only",children:s});export{e as V};
---
+++ .medusa/server/public/admin/assets/chunk-SNWWDVXZ-CsSxVxl6.js
@@ -0,0 +1,1 @@
+import{au as i}from"./index-4Ied8IC1.js";var a=i("product","*categories,*shipping_profile,-variants");export{a as P};
